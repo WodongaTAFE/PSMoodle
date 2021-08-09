ab4689c629dd8b74ce16622fdd80196ac29e81fd
Matt Hamilton <mhamilton@wodongatafe.edu.au>
2021-08-09 12:01:00 +1000

    Remove-MoodleUser

---
+++ Moodle/public/Remove-MoodleUser.ps1
@@ -0,0 +1,54 @@
+<#
+.SYNOPSIS
+Deletes a Moodle user.
+
+.PARAMETER Id
+Specifies the unique ID of the user to delete.
+
+.PARAMETER User
+Specifies the user to delete.
+
+.EXAMPLE
+Remove-MoodleUser -Id 1
+
+Deletes user #1.
+#>
+function Remove-MoodleUser {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory,Position=0,ParameterSetName='id',ValueFromPipelineByPropertyName)]
+        [int] $Id,
+
+        [Parameter(Mandatory,ParameterSetName='user',ValueFromPipelineByPropertyName)]
+        [MoodleUser] $User
+    )
+
+    Begin {
+        $Url = $PsCmdlet.SessionState.PSVariable.GetValue("_MoodleUrl")
+        $Token = $PsCmdlet.SessionState.PSVariable.GetValue("_MoodleToken")
+        
+        if (!$Url -or !$Token) {
+            Throw "You must call the Connect-Moodle cmdlet before calling any other cmdlets."
+        }
+
+        $function = 'core_user_delete_users'
+        $path = "webservice/rest/server.php?wstoken=$Token&wsfunction=$function&moodlewsrestformat=json"
+    }
+
+    Process {
+        if ($User) {
+            $Id = $User.Id
+        }
+
+        $body = @{
+            'userids[0]' = $Id
+        }
+
+        if ($PSCmdlet.ShouldProcess($Id, "Delete")) {
+            $result = Invoke-RestMethod -Method Post -Uri ([uri]::new($Url, $path)) -Body $body -ContentType 'application/x-www-form-urlencoded' 
+            if ($result.errorcode) {
+                Write-Error $result.message
+            }
+        }
+    }
+}
