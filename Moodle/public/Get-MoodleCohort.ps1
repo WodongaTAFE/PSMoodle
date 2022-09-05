231f23b65c7d0ffc39b49d7b21f494f7e9da1dba
Petri Asikainen <uniluodossa@gmail.com>
2022-09-05 07:39:42 +0300

    Feature/return all cohorts (#12)
    * new parameters for returning all results at once
    
    * Document parameters
    
    * Use parametset name instead of parameter name
    
    * Use parametset name instead of parameter name
    
    * Add includes parameter
    
    * Use parametset name instead of parameter name
    
    * Recursive calls for returning all results
    
    * Correct variable type
    
    * Debug statements
    
    * use contextid and specify includes with system context
    
    * Debug query paths
    
    * Dont be greedy
    
    * default parameterset is id even when piping other objects

--- Moodle/public/Get-MoodleCohort.ps1
+++ Moodle/public/Get-MoodleCohort.ps1
@@ -5,6 +5,43 @@ Gets a Moodle cohort.
 .PARAMETER Id
 Specifies the unique ID of the cohort.
 
+.PARAMETER System
+Query cohorts on system context.
+
+.PARAMETER System
+Query cohorts on system context.
+
+.PARAMETER Level
+Query cohorts on specified [MoodleContext] and.
+
+.PARAMETER InstanceID
+Query cohorts on specified [MoodleContext].
+
+.PARAMETER User
+Query cohorts on specified [MoodleUser].
+
+.PARAMETER Category
+Query cohorts on specified [MoodleCategory].
+
+.PARAMETER Course
+Query cohorts on specified [MoodleCourse].
+
+.PARAMETER Query
+Query cohorts with specified string.
+
+.PARAMETER LimitFrom
+Start results from this offset
+
+.PARAMETER LimitNum
+Limit numbers of results to spefied value 
+
+.PARAMETER All
+Return all results.
+
+.PARAMETER Includes 
+What other contexts to fetch the frameworks from. (all, parents, self)
+
+
 .EXAMPLE
 Get-MoodleCohort -Id  1
 
@@ -39,7 +76,36 @@ function Get-MoodleCohort {
         [Parameter(ParameterSetName='course')]
         [Parameter(ParameterSetName='level')]
         [Parameter(ParameterSetName='system')]
-        [string] $Query = ''
+        [string] $Query = '',
+
+        [Parameter(ParameterSetName='user')]
+        [Parameter(ParameterSetName='category')]
+        [Parameter(ParameterSetName='course')]
+        [Parameter(ParameterSetName='level')]
+        [Parameter(ParameterSetName='system')]
+        [int] $LimitFrom = 0,
+        
+        [Parameter(ParameterSetName='user')]
+        [Parameter(ParameterSetName='category')]
+        [Parameter(ParameterSetName='course')]
+        [Parameter(ParameterSetName='level')]
+        [Parameter(ParameterSetName='system')]
+        [int]$LimitNum = 25,
+
+        [Parameter(ParameterSetName='user')]
+        [Parameter(ParameterSetName='category')]
+        [Parameter(ParameterSetName='course')]
+        [Parameter(ParameterSetName='level')]
+        [Parameter(ParameterSetName='system')]
+        [switch]$All ,
+
+        [Parameter(ParameterSetName='user')]
+        [Parameter(ParameterSetName='category')]
+        [Parameter(ParameterSetName='course')]
+        [Parameter(ParameterSetName='level')]
+        [Parameter(ParameterSetName='id')]
+        [ValidateSet('all', 'parents', 'self')]
+        [String]$Includes = 'self'
     )
     
     Begin {
@@ -59,36 +125,59 @@ function Get-MoodleCohort {
     
     Process {
         $path = "webservice/rest/server.php?wstoken=$Token&wsfunction=$function&moodlewsrestformat=json"
+        $iterParams = @{}
 
-        if ($PSBoundParameters.ContainsKey('id')) {
-            $path += "&cohortids[0]=$Id"
-
-            $results = Invoke-RestMethod -Uri ([uri]::new($Url, $path))
-        }
-        else {
-            if ($System) {
-                $Level = [MoodleContext]::System
-                $InstanceId = 1
+        switch -Wildcard ($PsCmdlet.ParameterSetName) {
+            'id' {
+                $path += "&cohortids[0]=$Id"
+                Write-debug "Request path: $($path -replace 'wstoken=(.*?)&','wstoken=[hidden]&')"
+                $results = Invoke-RestMethod -Uri ([uri]::new($Url, $path))
+                Break
             }
-            elseif ($User) {
+            'level' {
+                $iterParams.Add('InstanceId', $InstanceId)
+                $iterParams.Add('Level', $Level)
+            }
+            'system' {
+                $Level = [MoodleContext]::System
+                $ContextId = 10
+                $iterParams.Add('System', $System)
+                $includes='parents'
+            }
+            'user'{
                 $Level = [MoodleContext]::User
                 $InstanceId = $User.Id
+                $iterParams.Add('User', $User)
             }
-            elseif ($Category) {
+            'category' {
                 $Level = [MoodleContext]::CourseCat
                 $InstanceId = $Category.Id
+                $iterParams.Add('Category', $Category)
             } 
-            elseif ($Course) {
+            'course' {
                 $Level = [MoodleContext]::Course
                 $InstanceId = $Course.Id
+                $iterParams.Add('Course', $Course)
             }
+            '*' {
+                $contextLevel = $Level.ToString().ToLower()
+                $path += "&context[contextlevel]=$contextLevel"
 
-            $contextLevel = $Level.ToString().ToLower()
-            $path += "&context[contextlevel]=$contextLevel&context[instanceid]=$InstanceId&includes=self&query=$Query"
+                if($InstanceID -ne '') {
+                    $path += "&context[instanceid]=$InstanceId"
+                }
 
-            $results = (Invoke-RestMethod -Uri ([uri]::new($Url, $path))).cohorts
+                if($ContextID) {
+                    $path += "&context[contextid]=$ContextId"
+                }
+
+                $path += "&includes=$Includes&limitfrom=$LimitFrom&limitnum=$LimitNum&query=$Query"
+                Write-debug "Request path: $($path -replace 'wstoken=(.*?)&','wstoken=[hidden]&')"
+                $results = (Invoke-RestMethod -Uri ([uri]::new($Url, $path))).cohorts
+            }
         }
-
+        
+        
         if ($results) {
             $results | Foreach-Object {
                 New-Object -TypeName MoodleCohort -Property @{
@@ -100,6 +189,15 @@ function Get-MoodleCohort {
                     Visible = $_.visible
                     Theme = $_.theme
                 } 
+            }
+            
+            #is there need to fetch more results
+            if($All -and $results.Count -eq $LimitNum ) {
+                $iterParams.Add('LimitFrom', $LimitFrom + $LimitNum)
+                $iterParams.Add('LimitNum', $LimitNum )
+                $iterParams.Add('All', $All)
+                Write-Debug "Call for more"
+                Get-MoodleCohort @iterParams
             }
         }
     }
