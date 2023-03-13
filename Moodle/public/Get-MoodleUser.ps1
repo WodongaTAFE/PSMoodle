bd711d391f4e1be90aecbf8d9ece13700c83e820
Johannes <126152533+jonahhes@users.noreply.github.com>
2023-03-13 02:36:31 +0100

    Let Get-MoodleUser take user ids from pipeline to work better with Get-MoodleCohortMember (#21)

--- Moodle/public/Get-MoodleUser.ps1
+++ Moodle/public/Get-MoodleUser.ps1
@@ -30,39 +30,39 @@ Searching for a user by email is case-sensitive, so we try the lower-case variat
 #>
 function Get-MoodleUser {
     # [OutputType([MoodleUserDetails])]
-    [CmdletBinding(DefaultParameterSetName='id')]
+    [CmdletBinding(DefaultParameterSetName = 'id')]
     param (
         # The unique user id.
-        [Parameter(ParameterSetName="id",Mandatory,Position=0)][int] $Id,
+        [Parameter(ParameterSetName = "id", Mandatory, Position = 0, ValueFromPipeline)][int] $Id,
         # The unique user name.
-        [Parameter(ParameterSetName="username",Mandatory)][string] $UserName,
+        [Parameter(ParameterSetName = "username", Mandatory)][string] $UserName,
         # The external "ID Number" of the user.
-        [Parameter(ParameterSetName="idnumber",Mandatory)][string] $IdNumber,
+        [Parameter(ParameterSetName = "idnumber", Mandatory)][string] $IdNumber,
         # THe unique email address of the user.
-        [Parameter(ParameterSetName="email",Mandatory)][string] $Email
+        [Parameter(ParameterSetName = "email", Mandatory)][string] $Email
     )
-    
+
     Begin {
         $Url = $Script:_MoodleUrl
         $Token = $Script:_MoodleToken
-        
+
         if (!$Url -or !$Token) {
             Throw "You must call the Connect-Moodle cmdlet before calling any other cmdlets."
         }
 
         $function = 'core_user_get_users_by_field'
     }
-    
+
     Process {
         $path = "webservice/rest/server.php?wstoken=$Token&wsfunction=$function&moodlewsrestformat=json"
-        
+
         $params = @{
-            id = $Id
+            id       = $Id
             username = $UserName
             idnumber = $IdNumber
-            email = $Email
+            email    = $Email
         }
-    
+
         $index = 0
         foreach ($key in $params.Keys) {
             $value = $params[$key]
@@ -76,20 +76,20 @@ function Get-MoodleUser {
             }
         }
 
-        $results = Invoke-RestMethod -Uri ([uri]::new($Url, $path)) 
+        $results = Invoke-RestMethod -Uri ([uri]::new($Url, $path))
         $results | Foreach-Object {
             New-Object -TypeName MoodleUserDetails -Property @{
-                Id=$_.id 
-                UserName = $_.username
-                Auth = $_.auth
-                FirstName = $_.firstname
-                LastName = $_.lastname
-                Email = $_.email
-                IdNumber = $_.idnumber 
-                Suspended = $_.suspended
+                Id          = $_.id
+                UserName    = $_.username
+                Auth        = $_.auth
+                FirstName   = $_.firstname
+                LastName    = $_.lastname
+                Email       = $_.email
+                IdNumber    = $_.idnumber
+                Suspended   = $_.suspended
                 FirstAccess = if ($_.firstaccess -gt 0) { [DateTimeOffset]::FromUnixTimeSeconds($_.firstaccess).DateTime } else { $null }
-                LastAccess = if ($_.lastaccess -gt 0) { [DateTimeOffset]::FromUnixTimeSeconds($_.lastaccess).DateTime } else { $null }
-            } 
+                LastAccess  = if ($_.lastaccess -gt 0) { [DateTimeOffset]::FromUnixTimeSeconds($_.lastaccess).DateTime } else { $null }
+            }
         }
     }
 }
